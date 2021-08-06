13dac59e4387608146be1d387a9a1094b2eaaa3c
Wickus Van der Merwe <vandermerwewickus@gmail.com>
2021-08-06 09:38:28 +0200

    Added contactn helper

--- src/i18n/json/index.tsx
+++ src/i18n/json/index.tsx
@@ -1,10 +1,54 @@
-import en from "../json/en.json";
-import fr from "../json/fr.json";
-import es from "../json/es.json";
 import LOCALES from "../locales.json";
+import PAGES from "../pages.json";
+// Add all the page content data HERE
+import homeContent from "./home";
+import contactContent from "./contact";
 
-export default {
-    [LOCALES.ENGLISH]: {...en},
-    [LOCALES.FRENCH]: {...fr},
-    [LOCALES.SPANISH]: {...es},
+interface IObject {
+    [key: string]: string | {};
+}
+
+interface IDictionary {
+    [key: string]: string;
+}
+
+const locales: IDictionary = LOCALES; // Making the json object type safe
+const pageNames: IDictionary = PAGES; // Making the json object type safe
+
+const languages = Object.keys(locales).map((item) => {
+    // Creating languages array
+    return locales[item];
+});
+
+const pages = Object.keys(pageNames).map((item) => {
+    // Creating pages array
+    return pageNames[item];
+});
+
+// Helper function to pass to the i18next resources section
+export const getResources = () => {
+    const resources: IObject = {};
+    // Looping through the languages to create a resource for each language
+    languages.map((lang) => {
+        const page: IObject = {};
+        // Looping through all the available pages to create a resource for each page using the data files
+        pages.map((pageName) => {
+            let pageContent = {};
+            // Assigning the correct data file to the correct page
+            switch (pageName) {
+                case "index":
+                    pageContent = homeContent[lang];
+                    break;
+                case "contact":
+                    pageContent = contactContent[lang];
+                    break;
+            }
+            // Setting the page content in the page object
+            page[pageName] = pageContent;
+        });
+        // Adding the page resources to be translated to the correct language
+        resources[lang] = { ...page };
+    });
+
+    return resources;
 };
